b46e1176fa42caf3b57ef7700dc5f6d1a66fde18
Shawn Blakesley <shawn@blakesley.io>
2020-06-30 16:35:09 -0700

    Replaced configure.js with gulp
    Now using gulp to build the project.
    Available commands:
    - `gulp`: Does a clean build of the project.
    - `gulp watch`: Watches files in `src` for changes.
    - `gulp dev`: Clean build followed by watching the files while
      serving them on localhost:8000.

--- README.md
+++ README.md
@@ -4,25 +4,35 @@ My personal resume website.
 
 Feel free to use as a starting place for your own site.
 
+## Requirements
+
+It's recommended to install gulp-cli before trying to build this site.
+
+```bash
+npm install -g gulp-cli
+```
+
 ## Installation
 
 Building the website is simple. Just run:
 
 ```bash
 npm install
-npm run build
+gulp
 ```
 
 To enter develoment mode, use:
 
 ```bash
-npm run open
+gulp dev
 ```
 
 This will do the following:
 
 1. Build the static files from the templates.
 
-1. Start an http-server to serve the newly created files.
+1. Watch those files for changes.
+
+1. Start a server running on localhost:8000 to serve the newly created files.
 
 1. Open the webpage in your default browser.
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var composer = require('gulp-uglify/composer');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var gulp = require('gulp');
+var path = require('path');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var uglifyjs = require('uglify-es');
+var webserver = require('gulp-webserver');
+var yaml = require('yamljs');
+
+var minify = composer(uglifyjs, console);
+
+sass.compiler = require('node-sass');
+
+let pug_data_file = path.join("src", "data", 'data.yml');
+
+
+function clean() {
+  return del('dist');
+}
+
+// function sass() {}
+
+function sass_task() {
+  return gulp.src('src/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('dist'));
+}
+
+function css_task() {
+  return gulp.src('src/**/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist'));
+}
+
+function js_min_task() {
+  return gulp.src(['src/**/*.js', '!src/**/*.min.js'])
+    .pipe(minify({}))
+    .pipe(gulp.dest('dist'));
+}
+
+function js_task() {
+  return gulp.src('src/**/*.min.js')
+    .pipe(gulp.dest('dist'));
+}
+
+function pug_task() {
+  let pug_locals = yaml.load(pug_data_file);
+  return gulp.src(['src/**/*.pug', '!src/includes/*.pug'])
+    .pipe(pug({
+      locals: pug_locals
+    }))
+    .pipe(gulp.dest('dist'));
+}
+
+function files_task() {
+  return gulp.src(['src/**', '!src/**/*.pug', '!src/**/*.scss', '!src/**/*.js', '!src/**/*.css'])
+    .pipe(gulp.dest('dist'));
+}
+
+function webserver_task() {
+  gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+}
+
+// exports.open = series(clean, parallel(sass, css, js, pug, files));
+exports.default = gulp.series(clean, gulp.parallel(css_task, sass_task, js_task, js_min_task, pug_task, files_task));
+
+function watch_all() {
+  gulp.watch('src/**/*.js', gulp.series(js_task, js_min_task));
+  gulp.watch('src/**/*.scss', sass_task);
+  gulp.watch('src/**/*.css', css_task);
+  gulp.watch(['src/**/*.pug', pug_data_file], pug_task);
+  gulp.watch(['src/**', '!src/**/*.pug', '!src/**/*.scss', '!src/**/*.js', `!${pug_data_file}`], files_task);
+};
+exports.watch = watch_all;
+
+exports.dev = gulp.series(exports.default, gulp.parallel(watch_all, webserver_task));
