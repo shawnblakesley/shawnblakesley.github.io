741c552212d509d5e901434dbc9ce5ab0cef2ca2
Shawn Blakesley <shawnblakesley@gmail.com>
2020-06-30 19:41:36 -0700

    Made deploy step manual

---
+++ tools/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# if no SERVER_IP raise warning
+if [ -z "${SERVER_IP}" ]; then
+  echo "You must set the SERVER_IP ${SERVER_IP} to point to the host"
+  exit
+fi
+cd $(dirname "$0")
+scp ./update_server.sh root@${SERVER_IP}:/root/update_server.sh
+ssh -t root@${SERVER_IP} ./update_server.sh $(echo https://index.docker.io/v1/ | docker-credential-osxkeychain get | jq ".Secret")
+
