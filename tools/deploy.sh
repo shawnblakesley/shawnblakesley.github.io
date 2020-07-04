e229af3c3b23e429c60303bf2d837fdf4543050a
Shawn Blakesley <shawnblakesley@gmail.com>
2020-07-03 21:18:35 -0700

    Add option to skip uploading images

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -6,11 +6,13 @@ if [ -z "${SERVER_IP}" ]; then
   exit
 fi
 cd $(dirname "$0")
-gulp images --compress-images
-pushd ../dist
-tar -czvf images.tar.gz images
-popd
-scp -r ../dist/images.tar.gz root@${SERVER_IP}:/root
+if [ -z "${SKIP_IMAGES}" ]; then
+  gulp images --compress-images
+  pushd ../dist
+  tar -czvf images.tar.gz images
+  popd
+  scp -r ../dist/images.tar.gz root@${SERVER_IP}:/root
+fi
 scp ./update_server.sh root@${SERVER_IP}:/root/update_server.sh
 ssh -t root@${SERVER_IP} ./update_server.sh $(echo https://index.docker.io/v1/ | docker-credential-osxkeychain get | jq ".Secret")
 
